d345b4701a7d3bb77e54f6b439a166641e9c2d54
Mister-0-Zero <alexyulyadkov@gmail.com>
2025-06-21 23:50:28 +0300

    the function of finding the right and left boundaries by binary search is implemented

--- start/binary_search/binary_search_left.py
+++ start/binary_search/binary_search_left.py
@@ -1,2 +1,15 @@
 def binary_search_left_f(arr: list[int], target: int) -> int:
-    pass
+    left = 0
+    right = len(arr) - 1
+    result = -1
+
+    while left <= right:
+        mid = (right + left) // 2
+        if arr[mid] < target:
+            left = mid + 1
+        else:
+            if arr[mid] == target:
+                result = mid
+            right = mid - 1
+            
+    return result
--- start/binary_search/binary_search_right.py
+++ start/binary_search/binary_search_right.py
@@ -1,2 +1,15 @@
 def binary_search_right_f(arr: list[int], target: int) -> int:
-    pass
+    left = 0
+    right = len(arr) - 1
+    result = -1
+
+    while left <= right:
+        mid = (right + left) // 2
+        if arr[mid] > target:
+            right = mid - 1
+        else:
+            if arr[mid] == target:
+                result = mid
+            left = mid + 1
+
+    return result
